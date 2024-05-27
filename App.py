d94c76cacf1d87ba3eef0099b33dd929cc897183
mrs-chavez <farrah.chavez@mrgs.school.nz>
2024-05-28 09:39:27 +1200

    Added comments to divide the different frames Added title Fixed the app exiting even when the user says no

--- App.py
+++ App.py
@@ -3,6 +3,12 @@ from app_settings import *
 from os import *
 from tkinter import messagebox 
 
+
+##### FONTS #######
+# Available TKinter fonts: https://stackoverflow.com/a/64301819
+
+title_font = ("Verdana", 30, "bold")
+
 class App():
 
     
@@ -11,30 +17,57 @@ class App():
         self.window.geometry(str(w_width) + "x" + str(w_height))
         self.window.title(app_title)
 
+########## TOP FRAME #############
+
         self.top_frame = Frame(background='red', width=w_width, height=100)
         self.top_frame.pack()
 
+        title_label = Label(text=app_title, font=title_font)
+        title_label.pack()
+
+
+########## END OF TOP #############
+
+
+########## MAIN FRAME #############
+
         self.main_frame = Frame(background=bg_color, width=w_width, height=(w_height-200))
         self.main_frame.pack()
+
+########## END OF MAIN #############
+
+########## BOTTOM FRAME #############
 
         self.bottom_frame = Frame(background='blue', width=w_width, height=100)
         self.bottom_frame.pack(side='bottom')
 
         self.home_button = Button(self.bottom_frame, text="Home", height=2, width=5, bg='green')
-        self.home_button.place(x=0,y=0)
+        self.home_button.pack(side='left')
 
         self.exit_button = Button(self.bottom_frame, text="Exit", height=2, width=5, bg='green', command= self.exit)
-        self.exit_button.place(x=100,y=0)
+        self.exit_button.pack(side='right')
 
+########## END OF BOTTOM #############
+
+
+########## OTHER APP SETUP #############
+
+        # Set relative path to the project folder
         self.dirname = path.dirname(__file__)
         self.filename = path.join(self.dirname, 'images/')
 
-        print("The path is", self.filename)
+        #print("The path is", self.filename)
+
 
         self.window.mainloop()
 
 
-
+    # Asks confirmation from user before exiting/destroying the app
     def exit(self):
-        if messagebox.askquestion("askquestion", "Are you sure?"):
+        confirm_exit = messagebox.askquestion("askquestion", "Are you sure?")
+        print(confirm_exit)
+        if confirm_exit == 'yes':
             self.window.destroy()
+        else:
+            print("Continue")
+            pass
