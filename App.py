65a83730097e5cee3874a7ca77b73b2db4b95784
mrs-chavez <farrah.chavez@mrgs.school.nz>
2024-05-30 13:08:10 +1200

    Added navigation functionality

--- App.py
+++ App.py
@@ -13,7 +13,8 @@ title_font = ("Verdana", 30, "bold")
 
 class App():
 
-    
+    current_frame = "Home"
+
     def __init__(self):
         self.window = Tk()
         self.window.geometry(str(w_width) + "x" + str(w_height))
@@ -24,9 +25,6 @@ class App():
         self.top_frame = Frame(background='red', width=w_width, height=100)
         self.top_frame.pack()
 
-        title_label = Label(text=app_title, font=title_font)
-        title_label.pack()
-
 
 ########## END OF TOP #############
 
@@ -36,10 +34,23 @@ class App():
         image = Image.open("imgs/house.png")
         photo = ImageTk.PhotoImage(image.resize((196, 196)))
 
-        self.main_frame = Frame(background=bg_color, width=w_width, height=(w_height-200))
-        self.main_frame.pack()
+        self.home_frame = Frame(background=bg_color, width=w_width, height=(w_height-200))
+        self.home_frame.pack()
 
-        bg_image = Label(image=photo)
+        self.title_label = Label(self.home_frame, text=app_title, font=title_font)
+        self.title_label.pack()
+
+        self.quiz_frame = Frame(background=bg_color, width=w_width, height=(w_height-200))
+
+        self.quiz_label = Label(self.quiz_frame, text="Quiz", font=title_font)
+        self.quiz_label.pack()
+
+        self.list_frame = Frame(background=bg_color, width=w_width, height=(w_height-200))
+
+        self.list_label = Label(self.list_frame, text="List", font=title_font)
+        self.list_label.pack()
+
+        bg_image = Label(self.home_frame, image=photo)
         bg_image.pack()
 
 
@@ -50,8 +61,14 @@ class App():
         self.bottom_frame = Frame(background='blue', width=w_width, height=100)
         self.bottom_frame.pack(side='bottom')
 
-        self.home_button = Button(self.bottom_frame, text="Home", height=2, width=5, bg='green')
+        self.home_button = Button(self.bottom_frame, text="Home", height=2, width=5, bg='green', command=lambda: self.go_to_frame("Home"))
         self.home_button.pack(side='left')
+
+        self.quiz_button = Button(self.bottom_frame, text="Quiz", height=2, width=5, bg='green', command=lambda: self.go_to_frame("Quiz"))
+        self.quiz_button.pack(side='left')
+
+        self.list_button = Button(self.bottom_frame, text="LIst", height=2, width=5, bg='green', command= lambda: self.go_to_frame("List"))
+        self.list_button.pack(side='left')
 
         self.exit_button = Button(self.bottom_frame, text="Exit", height=2, width=5, bg='green', command= self.exit)
         self.exit_button.pack(side='right')
@@ -80,3 +97,24 @@ class App():
         else:
             print("Continue")
             pass
+
+    def go_to_frame(self, next_frame):
+
+        if self.current_frame == "Home":
+                self.home_frame.pack_forget()
+        elif self.current_frame == "Quiz":
+                self.quiz_frame.pack_forget()
+        elif self.current_frame == "List":
+                self.list_frame.pack_forget()
+
+        if next_frame == "Quiz":
+                self.quiz_frame.pack()
+                self.current_frame = "Quiz"
+        elif next_frame == "List":
+                self.list_frame.pack()
+                self.current_frame = "List"
+        elif next_frame == "Home":
+                self.home_frame.pack()
+                self.current_frame = "Home"
+
+        
